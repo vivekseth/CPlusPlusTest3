fcfbd608489cff805faf55a5c6e31873a1fd8083
Vivek Seth <Manu@resnet-119-75.resnet.rutgers.edu>
2012-12-17 02:06:40 -0500

    Initial Commit

---
+++ CPlusPlusTest3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPlusPlusTest3
+//
+//  Created by Vivek Seth on 12/17/12.
+//  Copyright (c) 2012 Vivek Seth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
